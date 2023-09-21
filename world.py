d8d122fc8df16d76c259be7535c9ba347b9a87f6
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-09-21 11:23:24 +0200

    chgt sur develop

--- world.py
+++ world.py
@@ -1,4 +1,7 @@
 print('hello world')
 print ('vive python')
 print ('Bonjour VsCode - Como estas')
+
 print ('Bonjour VsCode - Como estas')
+
+print ('ceci est un commentaire sur la branche develop')
